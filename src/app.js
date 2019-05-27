6a45cb1e1f188822dc6aa0c37968374a28c82143
Jorge A <ardonz@gmail.com>
2019-05-27 16:48:34 -0600

    deployment changes for heroku

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Static content config
 app.use(express.static(path.join(__dirname, '../public')));
@@ -79,6 +80,6 @@ app.get('*', (req, res) => {
   })
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server running..')
 });
